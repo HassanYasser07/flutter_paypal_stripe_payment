366ed967ea8863f1a2d69841976fca7f95c8423c
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-06-23 19:05:07 +0300

    initial version

---
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_paypal_stripe_payment/core/utils/app_style.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+class MyCartView extends StatelessWidget {
+  const MyCartView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Center(child: SvgPicture.asset('assets/images/Arrow 1.svg')),
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+            'My Cart',
+          textAlign: TextAlign.center,
+          style: Styles.style25,
+
+        ),
+
+      ),
+
+    );
+  }
+}
