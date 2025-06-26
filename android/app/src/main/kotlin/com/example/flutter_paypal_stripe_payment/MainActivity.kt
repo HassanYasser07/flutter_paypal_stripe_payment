cc691f3ed6cdd5a8bdf8f38c863dab1ac0bf4703
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-06-26 18:24:05 +0300

    chore(android): update gradle, Kotlin, theme, and MainActivity to support flutter_stripe

--- android/app/src/main/kotlin/com/example/flutter_paypal_stripe_payment/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_paypal_stripe_payment/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.flutter_paypal_stripe_payment
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
