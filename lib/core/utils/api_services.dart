a8efcc38f273e1cc16b35610acfd01225f0b1f3a
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-06-28 20:05:52 +0300

    create api services (post request method)

---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final Dio dio = Dio();
+
+  Future<Response> post(
+      {required String url, required body, required String? token, required contentType}) async {
+    var response = await dio.post(url,
+        data: body,
+        options: Options(headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': contentType
+        }));
+    return response;
+  }
+}
